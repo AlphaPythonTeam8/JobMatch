9ccdbda4f2312d477b8efb3e83dfe49b5a81cf2d
easymneysniper <mariotelerikacademy@gmail.com>
2023-12-03 21:30:40 +0200

    Missed these...

--- routers/matches.py
+++ routers/matches.py
@@ -25,7 +25,7 @@ def send_match_request_to_company_ad(company_ad_id: int, company=Depends(oauth2.
                                      db: Session = Depends(get_db)):
     if not professional_services.get_ad(company_ad_id, db):
         raise HTTPException(status_code=404)
-    if match_services.get_company_ad_request(company, company_ad_id, db):
+    if match_services.get_company_ad_request(company.CompanyID, company_ad_id, db):
         raise HTTPException(status_code=400, detail='Match request is already sent.')
     return match_services.send_request_to_company_ad(company_ad_id, company.CompanyID, db)
 
@@ -42,10 +42,10 @@ def send_match_request_to_company(company_id: int, professional=Depends(oauth2.g
 @matches_router.post('/professional/{professional_id}')
 def send_match_request_to_professional(professional_id: int, company=Depends(oauth2.get_current_company),
                                        db: Session = Depends(get_db)):
-    match_request = match_services.get_professional_company_request(professional_id, company, db, 'Company')
+    match_request = match_services.get_professional_company_request(professional_id, company.CompanyID, db, 'Company')
     if match_request:
         raise HTTPException(status_code=400, detail='Match request already exists.')
-    return match_services.company_professional_request(company, professional_id, db, 'Company')
+    return match_services.company_professional_request(company.CompanyID, professional_id, db, 'Company')
 
 
 @matches_router.patch('/professional/{request_id}')
@@ -68,13 +68,13 @@ def professional_process_request(action: Annotated[str, Body(pattern='^accept$|^
 def company_process_request(action: Annotated[str, Body(pattern='^accept$|^reject')], request_id: int,
                             company=Depends(oauth2.get_current_company), db: Session = Depends(get_db)):
     match_request = match_services.match_request_exists_by_id(request_id, db)
-    if not match_services.company_own_request(company, match_request, db):
+    if not match_services.company_own_request(company.CompanyID, match_request, db):
         raise HTTPException(status_code=403)
     if not match_request or match_request.InitializedBy == 'Company':
         raise HTTPException(status_code=404)
     if not match_request.MatchStatus == 'Pending':
         raise HTTPException(status_code=400, detail='The match request is already processed by you.')
     if action == 'accept':
-        return match_services.company_accept_request(company, request_id, db)
+        return match_services.company_accept_request(company.CompanyID, request_id, db)
     else:
         return match_services.reject_match_request(request_id, db)
