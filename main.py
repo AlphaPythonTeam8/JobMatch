a2e98b38cd0392f0d49c97ffcb1ab61d48427e6f
AngelVMarinov <angel.v.marinov95@gmail.com>
2023-12-06 01:23:20 +0200

    admin router - imported in main

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
-from common.auth import auth_router, professional_auth_router
+from common.auth import auth_router, professional_auth_router, admin_auth_router
 from routers.companies import companies_router
 from routers.jobad import job_ad_router
 from routers.matches import matches_router
 from routers.professionals import professionals_router
 from routers.searching import searching_router
 from frontend.py.register import register_router
+from routers.admin import admin_router
 
 app = FastAPI()
 
@@ -23,9 +24,11 @@ app.add_middleware(
 )
 
 app.include_router(professionals_router)
+app.include_router(admin_router)
 app.include_router(companies_router)
 app.include_router(auth_router)
 app.include_router(professional_auth_router)
+app.include_router(admin_auth_router)
 app.include_router(searching_router)
 app.include_router(job_ad_router)
 app.include_router(matches_router)
